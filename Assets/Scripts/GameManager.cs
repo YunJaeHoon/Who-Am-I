2524501aa5b141f5892847a964d3978167965889
YunJaeHoon <sleepzzz214@naver.com>
2023-02-20 19:48:22 +0900

    Update(0.06)
    게임 씬 구현

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,27 +7,27 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    string[] WordArr = {"유재석","강호동","아이유","소녀시대","방탄소년단\n(BTS)","이병헌","박명수","김연아",
-                        "아이유","슈퍼주니어","잡초","농약","농부","과수원",
+    string[] WordArr = {"유재석","강호동","아이유","소녀시대","방탄소년단","이병헌","박명수","김연아",
+                        "아이유","슈퍼주니어","잡초","농약","농부","과수원","모자","엄마","아빠","슬리퍼",
                         "사도세자","송강호","세종대왕","이순신","오바마",
                         "원빈","윤봉길","김구","유관순","주몽",
                         "사과","오렌지","바나나","귤",
                         "수박","참외","두리안","자두","포도","블루베리","김치","배추","오이",
                         "당근","감자","고구마","토마토","치즈","우유","소세지","핫도그","식초","설탕","소금",
-                        "마요네즈","무\n(야채)","파\n(야채)","콜라","사이다","옥수수","커피","초콜렛",
+                        "마요네즈","무","파","콜라","사이다","옥수수","커피","초콜렛",
                         "사탕","뻥튀기","케이크","빵","치킨","피자","소","개","닭","쥐","호랑이",
-                        "사자","공작\n(공작새)","비둘기","참새","독수리","토끼","용","뱀","말",
-                        "양\n(동물)","원숭이","고릴라","돼지","에어컨","선풍기","전자레인지","냉장고","컴퓨터",
+                        "사자","공작새","비둘기","참새","독수리","토끼","용","뱀","말",
+                        "양","원숭이","고릴라","돼지","에어컨","선풍기","전자레인지","냉장고","컴퓨터",
                         "카메라","지뢰","복권","나무","꽃","연필","지우개","장미","튤립","해바라기","택시",
-                        "버스","별","우주선\n(로켓)","자전거","오토바이","사전","책","불꽃놀이","모래","흙","물",
-                        "불","용암","고무","석탄","철","금\n(황금)","다이아몬드","똥","오줌","도끼","망치","해머",
+                        "버스","별","로켓","자전거","오토바이","사전","책","불꽃놀이","모래","흙","물",
+                        "불","용암","고무","석탄","철","금","다이아몬드","똥","오줌","도끼","망치","해머",
                         "소화기","탱크","군인","경찰","소방서","경찰서","지뢰찾기","테트리스","리그오브레전드",
                         "오버워치","편의점","병원","약국","의사","지하철","기차","여의도","청와대","대통령",
                         "공원","휠체어","삼겹살","필통","세탁기","책상","의자","서울","부산","제주도","독도",
-                        "대한민국\n(한국)","일본","중국","미국","지구","우주","산","바다","화산","호주",
+                        "대한민국","일본","중국","미국","지구","우주","산","바다","화산","호주",
                         "유럽","아프리카","하와이","필리핀","몽골","이탈리아","프랑스","독일","태평양","백두산",
                         "요정","엘프","고블린","마법사","마녀","대머리","살인마","신","유령","귀신","천사",
-                        "악마","좀비","노예","피카츄","유비","관우","장비\n(삼국지)",
+                        "악마","좀비","노예","피카츄","유비","관우","장비",
                         "스폰지밥","뚱이","징징이","코난","건담","짱구","도라에몽",
                         "손오공","저팔계","사오정","해리포터","햄버거",
                         "파라오","스핑크스","피라미드","사막","오아시스","아령",
@@ -38,7 +38,7 @@ public class GameManager : MonoBehaviour
                         "화성",
                         "엘사",
                         "고양이","천둥","구름","번개","비",
-                        "거미","지네","개미","지렁이","파리\n(벌레)","조개","파도",
+                        "거미","지네","개미","지렁이","파리","조개","파도",
                         "스타벅스","레드벨벳","태연",
                         "유리","창문","조커","슈퍼맨","아이언맨",
                         "박쥐","배트맨",
@@ -55,7 +55,7 @@ public class GameManager : MonoBehaviour
                         "버섯","곰팡이","세균","현미경","돋보기","수학","과학","식당","숟가락","젓가락","포크",
                         "삼지창","철퇴","쇠사슬","레고","악어","귀뚜라미","메뚜기","피","영어","한글","한자",
                         "마블","깡패","야쿠자","코끼리","낙타","망토","지니","피아노","기타","리코더","빛",
-                        "바이올린","짜장면\n(자장면)","설거지","짬뽕","탕수육","양파","단무지","그림자",
+                        "바이올린","짜장면","설거지","짬뽕","탕수육","양파","단무지","그림자",
                         "지구온난화","올림픽","월드컵","축구","야구","농구","볼링","썰매","스케이트","낚시"};
 
     public Transform BackgounrdGroup;                               // 배경 축적 폴더
@@ -63,14 +63,25 @@ public class GameManager : MonoBehaviour
     public GameObject Background;                                   // 메인화면 배경
     public GameObject MainSceneGroup;                               // 메인씬 그룹
     public GameObject SubSceneGroup;                                // 서브씬 그룹
-    public GameObject ShowWords;                                    // 제시어 공개
+    public GameObject GameStartGroup;                               // 게임씬 그룹
+
+    public GameObject ShowWordsSlide;                               // 제시어 공개 슬라이드
+    public GameObject GameStartSlide;                               // 제시어 공개 슬라이드
     public GameObject ShowWordGroup;                                // 제시어씬 그룹
     public GameObject CheckWordGroup;                               // 제시어 가리는 그룹
+    public GameObject HintBtn;                                      // 힌트 버튼
+    public GameObject Hint_countCover;                              // 글자 수 힌트 가리개
+    public GameObject Hint_firstCover;                              // 초성 힌트 가리개
 
     public TMP_Text HeadCountUI;                                    // 인원수 숫자
     public TMP_Text NicknameTitle;                                  // 닉네임 + "제시어"
     public TMP_Text Warning;                                        // 닉네임 + "님을 제외한 플레이어만 확인하세요"
     public TMP_Text PlayerWord;                                     // 플레이어 제시어
+    public TMP_Text Round_text;                                     // 라운드
+
+    public TMP_Text NicknameQuestion;                               // 닉네임 + "님의 질문"
+    public TMP_Text Hint_count;                                     // 글자수 힌트
+    public TMP_Text Hint_first;                                     // 초성 힌트
 
     public GameObject[] Player = new GameObject[6];                 // 플레이어 창 배열
     public GameObject[] PlayerColorBtnArr = new GameObject[6];      // 플레이어 창의 색상 배열
@@ -89,6 +100,7 @@ public class GameManager : MonoBehaviour
     int CurrentColorBtn;                                    // 현재 누른 색상창의 색상 버튼
     int[] ColorArr = { 1, 2, 0, 0, 0, 0, 0, 0, 0, 0 };      // 색상 확인 배열
     int Who = 0;                                            // 플레이어 차례
+    int Round = 1;                                          // 게임 라운드
 
     bool isMain = true;                                     // 현재 메인 씬인가?
     bool isSub = false;                                     // 현재 서브 씬인가?
@@ -307,7 +319,7 @@ public class GameManager : MonoBehaviour
     {
         isSub = false;
         SubSceneGroup.SetActive(false);
-        ShowWords.SetActive(true);
+        ShowWordsSlide.SetActive(true);
         Invoke("DownShowWords", 1.5f);
         Background.SetActive(false);
 
@@ -327,12 +339,14 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    // 빨간색 제시어 확인 버튼
     public void CheckWord()
     {
         CheckWordGroup.SetActive(false);
         PlayerWord.text = Words[Who];
     }
 
+    // 초록색 확인 버튼
     public void CheckNext()
     {
         if(Who + 1 < HeadCount)
@@ -346,17 +360,82 @@ public class GameManager : MonoBehaviour
         }
         else
         {
+            ShowWordGroup.SetActive(false);
+            GameStartSlide.SetActive(true);
 
+            Invoke("DownGameStart", 1.5f);
         }
     }
 
-    // 제시어 공개 닫기
+    // 글자 수 힌트 버튼
+    public void ShowHintCount()
+    {
+        Hint_countCover.SetActive(false);
+        HintBtn.transform.GetChild(0).gameObject.SetActive(false);
+    }
+
+    // 초성 힌트 버튼
+    public void ShowHintFirst()
+    {
+        Hint_firstCover.SetActive(false);
+        HintBtn.transform.GetChild(1).gameObject.SetActive(false);
+    }
+
+    public void ChallengeSkip()
+    {
+        if (Who + 1 < HeadCount)
+        {
+            Who++;
+
+            Backgrounds[Who-1].SetActive(false);
+            Backgrounds[Who].SetActive(true);
+            NicknameQuestion.text = Nickname[Who] + "\n님의 질문";
+            Hint_count.text = Words[Who];
+            Hint_first.text = Words[Who];
+            HintBtn.SetActive(false);
+        }
+        else
+        {
+            Who = 0;
+
+            Backgrounds[HeadCount-1].SetActive(false);
+            Backgrounds[Who].SetActive(true);
+            NicknameQuestion.text = Nickname[Who] + "\n님의 질문";
+            Hint_count.text = Words[Who];
+            Hint_first.text = Words[Who];
+            HintBtn.SetActive(true);
+
+            Round++;
+            Round_text.text = "Round " + Round;
+        }
+    }
+
+    // 제시어 공개 슬라이드 닫기
     void DownShowWords()
     {
-        ShowWords.SetActive(false);
         Backgrounds[0].SetActive(true);
         ShowWordGroup.SetActive(true);
+
         NicknameTitle.text = Nickname[0] + "\n님의 제시어";
         Warning.text = Nickname[0] + " 님을\n 제외한 플레이어만 확인하세요";
+
+        ShowWordsSlide.SetActive(false);
+    }
+
+    // 게임 시작 슬라이드 닫기
+    void DownGameStart()
+    {
+        Backgrounds[Who].SetActive(false);
+        Who = 0;
+        Backgrounds[Who].SetActive(true);
+
+        NicknameQuestion.text = Nickname[Who] + "\n님의 질문";
+        Hint_count.text = Words[Who];
+        Hint_first.text = Words[Who];
+
+        GameStartGroup.SetActive(true);
+        Round_text.text = "Round " + Round;
+
+        GameStartSlide.SetActive(false);
     }
 }
